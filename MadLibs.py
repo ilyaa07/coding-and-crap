53bfb59a58566f1d88237b348868150c11596421
ilyaa07 <90723091+ilyaa07@users.noreply.github.com>
2021-09-15 00:11:41 +0300

    Create MadLibs.py

---
+++ MadLibs.py
@@ -0,0 +1,11 @@
+
+print("}MAD LIBS{")
+
+color = input("Pick a color: ")
+plural_noun = input("Pick a plural noun: ")
+someone = input("Pick someone you like: ")
+
+print("====================")
+print("Roses are " +color+"")
+print(""+plural_noun+ " are blue")
+print("I love " +someone+ "")
